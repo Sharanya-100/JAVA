748bfba2fc6a1dc21049948d9173982dc9692db6
Sharanya-100 <sharanyachowdhury30@gmail.com>
2025-01-06 12:58:04 +0530

    NA

---
+++ cses1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class cses1 {
+    public static void main (String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a non-negative number:");
+        int a=sc.nextInt();
+        if(a<=0){
+            System.out.println("enter a positive integer");
+        }
+        else{
+            System.out.println("Sequence is:");
+        
+
+        while(a!=1){
+            System.out.print(a+" ");
+            if(a%2==0){
+                a/=2;
+            }
+            else{
+                a=3*a+1;
+            }
+        }
+        System.out.print(a);
+    }
+}
+}
